2a573cde87308e9b8a88bd1fa06572d140798d4d
namwebdev <nam.job96@gmail.com>
2021-05-22 23:16:40 +0700

    - Edit PORT var

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { rootRouter } = require("./routes/index");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
